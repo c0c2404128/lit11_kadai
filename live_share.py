28f2c0d104f0e6f37f822d7dd69dc1beeafd8ddb
c0c2404128 <c0c2404128@edu.teu.ac.jp>
2024-06-14 18:05:46 +0900

    がんばってます

--- live_share.py
+++ live_share.py
@@ -3,5 +3,6 @@ b = ("c0c24029") #c0c24029
 ryo = int(a[6:]) #c0c24041
 ko = int(b[6:]) #c0c24029
 ans = ryo+ko #c0c24041
-print("変数１と変数２を足すと"+ans+"になります")#c0c24029
-
+print("変数１と変数２を足すと"+str(ans)+"になります")#c0c24029
+ans2 = ryo*ko
+print("変数１と変数２かけると"+str(ans2)+"になります")
